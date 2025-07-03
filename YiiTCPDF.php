76846dab3f9daa876c1e3fc04504637732e6735c
Ade Priyanto <mr.adepriyanto@gmail.com>
2025-07-03 10:06:52 +0700

    Update lib

--- YiiTCPDF.php
+++ YiiTCPDF.php
@@ -7,7 +7,7 @@ require_once(dirname(__FILE__) . '/tcpdf/tcpdf.php');
 /**
  * This is just an example.
  */
-class YiiTCPDF extends \yii\base\Widget
+class YiiTCPDF extends \TCPDF
 {
     public function __construct($orientation = 'P', $unit = 'mm', $format = 'A4', $unicode = true, $encoding = 'UTF-8', $diskcache = false, $pdfa = false) {
         parent::__construct($orientation, $unit, $format, $unicode, $encoding, $diskcache, $pdfa);
